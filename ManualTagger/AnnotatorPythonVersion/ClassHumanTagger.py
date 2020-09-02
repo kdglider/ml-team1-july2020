e7c791b8e91350646f1ffc65a1e9e8f640e9d53e
Kitty Gu <kittyguz@berkeley.edu>
2020-09-01 23:08:23 -0500

    Possibly the final draft for annotator integration

--- ManualTagger/AnnotatorPythonVersion/ClassHumanTagger.py
+++ ManualTagger/AnnotatorPythonVersion/ClassHumanTagger.py
@@ -3,8 +3,9 @@ import pandas as pd
 import numpy as np
 from openpyxl import Workbook, load_workbook
 import SessionState
+import time
 
-class MannualTagger:
+class ManualTagger:
     df_posts = None
     data_dic = None
     iterate_key = None
@@ -13,20 +14,17 @@ class MannualTagger:
     df_tags = None
     tags_list = None
     df_labeled = None
-    
+     
     def __init__(self):
         self.df_posts
-        #self.df_posts.to_csv("Initial_Data.csv")
 
         self.data_dic
         self.iterate_key
         self.curr_key
         self.session
-        #importing tags list
-        self.df_tags = pd.read_csv("Tags.csv")
+        self.df_tags = pd.read_csv("Tags.csv") #Gets the list of possible tags
         self.tags_list = self.df_tags.Tags.tolist()
-
-        self.df_labeled = pd.read_csv('LabeledData.csv')
+        self.df_labeled = pd.read_csv('LabeledData.csv') #Reads in the initial empty labeled data files
     """
     This function takes in a dataframe and returns a dictionary with the topic title as key and the leading comment as value
     """
@@ -38,12 +36,7 @@ class MannualTagger:
             for row in range(len(df)):
                 dic[df['Topic Title'].iloc[row]] = df['Leading Comment'].iloc[row]
             return dic
-
-        """
-        These two statements read in our desired csv files. The first one reads in the file that contains all redefined TagNames, which we have to make ourselves. The second contains scrapped data.
-        """
-        #new_labeled_topics: is the dataframe where we store labeled data
-        #new_labeled_topics = self.df_posts.copy()
+             
         #data_dic: contains the the scrapped data: with the topic title as key and the leading comment as value
         self.data_dic = get_data_from_file(self.df_posts)
         #iterate_key: an iterater that goes to the next topic when called next(iterate_key)
@@ -60,42 +53,47 @@ class MannualTagger:
                     """)
             return self.df_labeled
         else:
-            """
-            Below is mainly code for StreamLit display.
-            """
+            #twoRounds = 1
+            #while twoRounds <= 2:
+                #Below is mainly code for StreamLit display.
             st.write("""
             ** ML July Team1 Manual Tagging App**
             """)
-
-            #the line below is for the drop down menu for tag selection. We will switch df_posts with df_all_tag.
             self.load_data(self.df_posts)
-
             #remove the tagged post from the dataset and reset
-            if st.button("Reset"):
+             
+            st.write("**_Topic Title:_**")
+            st.write(self.curr_key)
+
+            st.write("**_Leading Comment:_**")
+            st.write(self.data_dic[self.curr_key])
+             
+            self.session = SessionState.get(run_id=0)
+            options = st.multiselect('Please select suitable tags for the above topic.', self.tags_list, key=self.session.run_id)
+            st.write('**_You have selected:_**', options)
+
+            #writes the tagged post to a excel file and removes the tagged post from the dataset and reset
+            st.write("""
+            ** IMPORTANT: Once you hit "Submit" the label is perminant. Please make sure your decision is final before hitting submit! You have 2 minutes to complete the current topic tagging**
+            """)
+            if st.button("Submit"):
+                st.write("**You have submitted your choices**")
+                row_to_append = pd.DataFrame({self.df_labeled.columns[0]: [self.curr_key], self.df_labeled.columns[1]: [self.data_dic[self.curr_key]], self.df_labeled.columns[2]: [options]})
+                self.df_labeled = self.df_labeled.append(row_to_append)
+                self.df_labeled.to_csv('LabeledData.csv', index=False)
                 df_posts = pd.read_csv("StackOverflow_new_tags.csv")
                 df_posts = df_posts.iloc[1:]
                 df_posts.to_csv("StackOverflow_new_tags.csv")
-
+                 
             #displays next topic
             self.session = SessionState.get(run_id=0)
             if st.button("Next Topic"):
+                st.write("**Going to the next topic**")
                 self.session.run_id += 1
-
-            options = st.multiselect('Please select suitable tags for the following topic.', self.tags_list, key=self.session.run_id)
-            st.write('You selected:', options)
-
-            st.write("Topic Title:")
-            st.write(self.curr_key)
-
-            st.write("Leading Comment:")
-            st.write(self.data_dic[self.curr_key])
-
-            #writes the tagged post to a excel file
-            if st.button("Submit"):
-                row_to_append = pd.DataFrame({self.df_labeled.columns[0]: [self.curr_key], self.df_labeled.columns[1]: [self.data_dic[self.curr_key]], self.df_labeled.columns[2]: [options]})
-                self.df_labeled = self.df_labeled.append(row_to_append)
-                df.to_csv('LabeledData.csv', index=False)
-         
-        
-
+                 
+            #time.sleep(10)
+            #st.write("""
+            #** Sleep Time Over! **
+            #""")
+            #twoRounds += 1
 
--- ManualTagger/AnnotatorPythonVersion/annotator.py
+++ ManualTagger/AnnotatorPythonVersion/annotator.py
@@ -1,8 +1,11 @@
-from classifier import Classifier
-from TagPredictor.classifier_SVM import Classifier_SVM
+'''
+@file       Annotator.py
+@date       2020/08/25
+@brief      Top level class that defines the annotation tool and active learning algorithm
+'''
+from TagPredictor.multilabelclassifier_SVM import MultilabelClassifier_SVM
 from TagPredictor.TagPredictor import TagPredictor
-from ClassHumanTagger import MannualTagger
-
+from ClassHumanTagger import ManualTagger
 import nltk
 nltk.download('stopwords')
 from nltk.corpus import stopwords
@@ -13,12 +16,16 @@ import numpy as np
 import pandas as pd
 
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MultiLabelBinarizer
+from sklearn.metrics import hamming_loss
+from sklearn.metrics import accuracy_score
 
-'''
-@file       Annotator.ipynb
-@date       2020/08/03
-@brief      Top level class that defines the annotation tool and active learning algorithm
-'''
+# Set Pandas display options
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_colwidth', 20)
+pd.set_option('display.width', None)
+pd.set_option('display.expand_frame_repr', False)   # Disable wrapping
 
 
 '''
@@ -27,22 +34,25 @@ from sklearn.model_selection import train_test_split
 class Annotator:
     groundTruthDB = None            # Pandas dataframe of all data with ground truth labels
     labeledDB = None                # Pandas dataframe of labeled data
-    unlabeledDB = None                # Pandas dataframe of unlabeled data
+    unlabeledDB = None              # Pandas dataframe of unlabeled data
 
     tagPredictor = None             # TagPredictor object
     manualTagger = None             # ManualTagger object
 
-    confidenceThreshold = 0.8       # Prediction confidence threshold to determine if a topic should be passed to ManualTagger
+    confidenceThreshold = 0.95      # Prediction confidence threshold to determine if a topic should be passed to ManualTagger
 
 
+    '''
+    @brief      Class constructor
+    @param      datafile    CSV dataset file that contains topic text and tags
+    @return     None
+    '''
     def __init__(self, datafile):
         # Create databases
-        self. groundTruthDB = pd.read_csv(datafile)
-
-        self.labeledDB, self.unlabeledDB = self.createDatabases(datafile)
+        self.groundTruthDB, self.labeledDB, self.unlabeledDB = self.createDatabases(datafile)
 
         # Set up ManualTagger
-        manualTagger = MannualTagger()
+        self.manualTagger = ManualTagger()
     
 
     '''
@@ -51,6 +61,7 @@ class Annotator:
     @return     text    String that contains the cleaned sentence
     '''
     def cleanText(self, text):
+        # Function that checks if all characters in a string are ASCII
         def is_ascii(s):
             return all(ord(c) < 128 for c in s)
         
@@ -63,13 +74,6 @@ class Annotator:
 
         # Convert all letters to lowercase
         text = text.lower()
-        
-        # Strip all punctuation
-        #table = str.maketrans('', '', string.punctuation)
-        #text = text.translate(table)
-
-        # Remove all non-ASCII characters
-        #text = text.encode(encoding='ascii', errors='ignore').decode('ascii')
 
         # Split feature string into a list to perform processing on each word
         wordList = text.split()
@@ -85,17 +89,7 @@ class Annotator:
         punctuation = '!"#%&\'()*+,-./:;<=>?@[\\]^_`{|}~'
         wordList = [word.strip(punctuation) for word in wordList]
 
-        # Replace all numbers with ######## identifier
-        # Replace all costs with $$$$$$$$ identifier
-        wordList = ['########' if (word.replace('.','').isdigit()) \
-                    else '$$$$$$$$' if (word.replace('.','').replace('$','').isdigit()) \
-                    else word \
-                    for word in wordList]
-        #wordList = ['########' if (word.replace('.','').isdigit()) else word for word in wordList]
-        #wordList = ['########' if (word.translate(table).isdigit()) else word for word in wordList]
-
         # Reconstruct text
-        # If it is empty, do not add this sample to the final output
         text = ' '.join(wordList)
 
         return text
@@ -110,18 +104,17 @@ class Annotator:
     '''
     def createDatabases(self, datafile):
         # Load CSV file as ground truth database
-        groundTruthDB = self.groundTruthDB
+        groundTruthDB = pd.read_csv(datafile)
 
         # Combine topic title and leading comment columns
         groundTruthDB['Bag_of_Words'] = groundTruthDB['Topic Title'] + groundTruthDB['Leading Comment']
         groundTruthDB['Bag_of_Words'] = groundTruthDB['Bag_of_Words'].str.strip().str.replace('   ', ' ').str.replace('  ', ' ')
-
-        groundTruthDB = groundTruthDB.drop(columns=['Topic Title', 'Leading Comment', 'Unnamed: 0'])
         
-        # Apply cleanText() to all columns with this:
+        # Apply cleanText() to the bag of words
         groundTruthDB['Bag_of_Words'] = groundTruthDB['Bag_of_Words'].apply(lambda x: self.cleanText(x))
 
-
+        # Code to duplicate multi-tag topics if necessary
+        '''
         #create an offset value
         offset = 0
         #the total number of unique comments
@@ -144,27 +137,32 @@ class Annotator:
             if (isinstance(groundTruthDB.loc[index, 'Tags'], list)):
                 groundTruthDB.loc[index, 'Tags'] = ''
                 # Not sure why this element is stored as '[]' instead of ''
+        '''
 
         # Filter out topics with no tags
         groundTruthDB = groundTruthDB[groundTruthDB['Tags'].map(len) > 2]
 
+        # Convert Tag column elements from strings to lists
+        groundTruthDB['Tags'] = groundTruthDB.Tags.apply(lambda x: x[1:-1].split(','))
+
+        # Take only a subset of the full dataset, if necessary
+        #groundTruthDB = groundTruthDB.sample(1000)
+
         # Split ground truth database into labeled and unlabelled databases
-        #mask = np.random.rand(len(groundTruthDB)) < 0.8
-        #labeledDB = groundTruthDB[~mask]
-        #unlabeledDB = groundTruthDB[mask]['Bag_of_Words']
-
         unlabeledDB, labeledDB = train_test_split(groundTruthDB, test_size=0.2)
-        unlabeledDB = unlabeledDB['Bag_of_Words']
 
-        return labeledDB, unlabeledDB
+        return groundTruthDB, labeledDB, unlabeledDB
 
 
     '''
     @brief      Demonstration function to run the entire annotator application
-    @param      
+    @param      classifier      Scikit-learn like classifier class to be used
     @return     None
     '''
     def runApplication(self, classifier):
+        # Create multilabel binarizer for metric calculations
+        mlb = MultiLabelBinarizer()
+
         # Set up TagPredictor object
         tagPredictor = TagPredictor(classifier, self.labeledDB)
 
@@ -172,42 +170,61 @@ class Annotator:
         tagPredictor.train()
 
         # Predict tags for all unlabeled topics
-        tagList, confidenceList = tagPredictor.predict(self.unlabeledDB)
+        tagList, confidenceList = tagPredictor.predict(self.unlabeledDB['Bag_of_Words'])
 
         # Continue running the active learning loop as long as there are still low-confidence topics
+        counter = 1
+        print('Minimum Confidence: ', min(confidenceList))
+        print('Maximum Confidence: ', max(confidenceList))
         while (any(p < self.confidenceThreshold for p in confidenceList) == True):
-            # Log tagging statistics
+            # Print out active learning statistics
+            print('Active Learning Iteration ', counter)
+            print('Labeled Database Size: ', len(self.labeledDB))
+            print('Unlabeled Database Size: ', len(self.unlabeledDB))
+            trueLabelIndicatorMatrix = mlb.fit_transform(self.unlabeledDB['Tags'])
+            predictedLabelIndicatorMatrix = mlb.transform(tagList)
+            print('Hamming Loss: ', hamming_loss(trueLabelIndicatorMatrix, predictedLabelIndicatorMatrix))
+            print('Accuracy: ', accuracy_score(trueLabelIndicatorMatrix, predictedLabelIndicatorMatrix))
             
             # Get low-confidence topic indices
-            lowConfIndices = [i for i in range(len(L)) if confidenceList[i] < self.confidenceThreshold]
+            lowConfIndices = [i for i in range(len(confidenceList)) if confidenceList[i] < self.confidenceThreshold]
 
             # Pass low-confidence topics to the manual tagger
-            lowConfTopics = self.unlabelDB.iloc(lowConfIndices)
+            lowConfTopics = self.unlabeledDB.iloc[lowConfIndices]
             labeledTopics = self.manualTagger.run(lowConfTopics)
-
+            print("Going on")
             # Add manually tagged topics to the labeled database
             self.labeledDB = pd.concat([self.labeledDB, labeledTopics], join='inner')
 
             # Remove tagged topics from unlabeled database
-            self.unlabeledDB = self.unlabeledDB.drop(lowConfTopics)
+            #old = len(self.unlabeledDB)
+            cond = self.unlabeledDB['Bag_of_Words'].isin(lowConfTopics['Bag_of_Words'])
+            self.unlabeledDB.drop(self.unlabeledDB[cond].index, inplace=True)
+            #print(old - len(self.unlabeledDB))
+            #print(len(lowConfTopics))
 
-            # Train tagPredictor with updated database
+            # Exit active learning loop if there are no more topics in the unlabeled database
+            if (len(self.unlabeledDB) == 0):
+                break
+
+            # Train tagPredictor with updated labeled database
             tagPredictor = TagPredictor(classifier, self.labeledDB)
             tagPredictor.train()
 
             # Predict tags for all unlabeled topics
-            tagList, confidenceList = tagPredictor.predict(self.unlabeledDB)
+            tagList, confidenceList = tagPredictor.predict(self.unlabeledDB['Bag_of_Words'])
+
+            counter += 1
+
+
 
 if __name__ == '__main__':
-
     # Path to CSV datafile
-    datafile = '/Users/kittyguz/Desktop/AnnotatorPythonVersion/StackOverflow_new_tags.csv'
-    df = pd.read_csv(datafile)
-    manualTagger = MannualTagger()
-    result = manualTagger.run(df)
+    datafile = 'StackOverflow_new_tags.csv'
 
-    #print(annotator.groundTruthDB)
+    # Instantiate Annotator object
+    annotator = Annotator(datafile)
 
-    #text = annotator.groundTruthDB.iloc[96]['Bag_of_Words']
-    #print(text)
-    #print(annotator.cleanText(text))
+    # Run annotation application
+    annotator.runApplication(MultilabelClassifier_SVM)
+
